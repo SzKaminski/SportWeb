7a2887b1138ff4fdc906d89d989ec3610400ced3
SzKaminski <46759012+SzKaminski@users.noreply.github.com>
2021-12-15 17:09:30 +0100

    update

---
+++ src/main/java/com/szkaminski/sportweb/SportwebApplication.java
@@ -0,0 +1,13 @@
+package com.szkaminski.sportweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szkaminski/sportweb/SportwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szkaminski.sportweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
